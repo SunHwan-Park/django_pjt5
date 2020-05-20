897e523c04c552fa48db04db86d69df061f3c225
SunHwan-Park <meshim435@naver.com>
2020-05-20 16:32:52 +0000

    update wsgi.py

--- django_pjt5/wsgi.py
+++ django_pjt5/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_pjt4.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_pjt5.settings')
 
 application = get_wsgi_application()
