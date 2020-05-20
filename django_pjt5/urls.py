7068d6a2ca7c0c946bb2a1bfc089eb5a8828e02d
SunHwan-Park <meshim435@naver.com>
2020-05-20 16:21:59 +0000

    update wsgi.py

--- django_pjt5/urls.py
+++ django_pjt5/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from movies import views
+from movies import views as movies_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('movies/', include('movies.urls')),
-    path('', views.index),
+    path('', movies_views.index),
 ]
